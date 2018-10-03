1b47e6ef877977fedc99e1648850e54f2a5b83c9
tamminen.mikkoj <tamminen.mikkoj@gmail.com>
2018-10-03 14:03:59 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,11 +5,10 @@ def key_for_min_value(name_hash)
   key_of_lowest_value = nil
   value_of_lowest_value = nil
     name_hash.each do |key, value|
-      if key_of_lowest_value = nil
+      if key_of_lowest_value == nil
         key_of_lowest_value = key
         value_of_lowest_value = value
-      end
-     elsif value < value_of_lowest_value
+      elsif value < value_of_lowest_value
        value_of_lowest_value = value
        key_of_lowest_value = key
      end
